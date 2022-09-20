ddc44caa3d4006791a07d7fa77281fcacfb415ca
Seb <samm1497@hotmail.com>
2022-09-20 02:04:10 +0000

    added logo

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'images')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
