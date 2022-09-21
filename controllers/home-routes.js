1c2f009fa601b024fa30d71786fb47db4fa53248
Seb <samm1497@hotmail.com>
2022-09-21 01:44:35 +0000

    routing for categories

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -23,6 +23,50 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/Misc", (req, res) => {
+  Category.findAll({
+    where: {
+      category_name: "Misc"
+    },
+    include: [
+      {
+        model: Material,
+        attributes: ["id", "material_name", "available", "student_id"],
+      },
+    ],
+  })
+    .then((dbCategoryData) => {
+      const categories = dbCategoryData.map(category => category.get({ plain: true }))
+      console.log(categories.material)
+      res.render('homepage',{ categories })
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// router.get("/Misc", (req, res) => {
+//   Material.findAll({
+//     attributes: ["id", "material_name", "available", "student_id"],
+//     include: [
+//       {
+//         model: Category,
+//         attributes: ["category_name", "id"],
+//       },
+//     ],
+//   })
+//     .then((dbMaterialData) => {
+//       const materials = dbMaterialData.map(material => material.get({ plain: true }))
+//       console.log(materials)
+//       res.render('homepage',{ materials })
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
 router.get('/register', (req, res) => {
     res.render('register');
   });
