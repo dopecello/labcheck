21259130d1ab57b4f920836fb1ca1e12e3b51c1a
Sebastian Melendez <36426874+SebasMelendez@users.noreply.github.com>
2022-09-24 12:05:51 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.JAWSDB_URL,
+    host: process.env.HOSTNAME,
     dialect: "mysql",
     port: 3306,
   }
